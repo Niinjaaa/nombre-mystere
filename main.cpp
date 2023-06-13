d528339221409282745b325393dc3315a07e9adb
Ninja <nicolas.herissard@orange.fr>
2023-06-13 22:47:48 +0200

    Nombre mystère

--- main.cpp
+++ main.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
-#include <math.h>
+#include <cstdlib>
+#include <random>
 
 using namespace std;
 
-int nb_triangle(int n)
-{
-    int result = n * (n + 1) / 2;
-    return result;
-}
-
 int main() {
 
-    int a = 0;
-    int b = 0;
+    srand(time(nullptr));
+    int value = (rand() % (300 - 100) + 100);
+    int choice;
+    cout << "Entrez un nombre" << endl;
+    cin >> choice;
 
-    for(int i = 0; i < 20; i++)
+    while (choice != value)
     {
-        a = nb_triangle(i + 1);
-        b = nb_triangle(i + 2);
-
-        cout << "T" << i + 1 << " = " << int(a) << endl;
-        cout << "T" << i + 2 << " = " << int(b) << endl;
-
-        if(sqrt(a + b) == int(sqrt(a + b)))
+        if(choice < value)
         {
-            cout << "carré : " << int(a + b) << " coté " << int(sqrt(a + b)) << "x" << int(sqrt(a + b)) << endl;
+            cout << "c'est plus" << endl;
+            cin >> choice;
+        }
+        else if(choice > value)
+        {
+            cout << "c'est moins" << endl;
+            cin >> choice;
         }
     }
+
+    cout << "Bravo le nombre etait : " << value << endl;
 
     return 0;
 }
